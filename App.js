20ac0dd5c5cd456796eb441a3a70cb75439dc866
muhammadsarim555 <muhammadsarim555@gmail.com>
2020-07-31 13:34:34 +0500

    Handle Input & Flex Containarise

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
+  TextInput,
   StyleSheet,
   ScrollView,
   View,
@@ -29,72 +29,49 @@ const {width, height} = Dimensions.get('window');
 const App: () => React$Node = () => {
   return (
     <View style={{flex: 1}}>
-      <View style={{flex: 1, backgroundColor: 'orange'}}></View>
-      <View style={{flex: 8, justifyContent: 'center'}}>
+      <View
+        style={{
+          backgroundColor: 'orange',
+        }}>
+        <Text style={{fontSize: 16, padding: 15}}>Header</Text>
+      </View>
+      <View style={{flex: 10, justifyContent: 'center'}}>
         <View
           style={{
-            height: height * 0.7,
+            flex: 1,
             backgroundColor: 'pink',
-            alignItems: 'center',
-            justifyContent: 'space-around',
           }}>
           <View
             style={{
-              width: width * 0.3,
-              height: height * 0.2,
-              borderRadius: 60,
-              backgroundColor: 'blue',
-            }}></View>
+              flex: 2,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}>
+            <View
+              style={{backgroundColor: 'blue', padding: 70, borderRadius: 50}}
+            />
+          </View>
           <View
             style={{
-              width: width * 0.7,
-              height: height * 0.4,
-              backgroundColor: 'green',
-            }}></View>
+              flex: 3,
+            }}>
+            <View style={{backgroundColor: 'red', margin: 20}}>
+              <TextInput />
+            </View>
+            <View
+              style={{backgroundColor: 'yellow', flex: 1, margin: 20}}></View>
+          </View>
         </View>
       </View>
-      <View style={{flex: 1, backgroundColor: 'purple'}}></View>
+
+      <View
+        style={{
+          backgroundColor: 'orange',
+        }}>
+        <Text style={{fontSize: 16, padding: 15}}>Footer</Text>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
